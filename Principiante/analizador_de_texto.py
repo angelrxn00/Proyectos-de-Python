dc0391b3a2537f9b3c3b6438d2699590d4545e7f
angelrxn00 <128822525+angelrxn00@users.noreply.github.com>
2025-01-03 22:13:04 +0100

    Update analizador_de_texto.py

--- Principiante/analizador_de_texto.py
+++ Principiante/analizador_de_texto.py
@@ -11,20 +11,19 @@ El objetivo del proyecto es que, mediante un texto cualquiera que el usuario int
     - Y, por último, si en el texto aparece la palabra 'python'
 
 """
-# Le decimos al usuario que escriba un texto cualquiera para operar sobre él.
-texto_a_introducir = input("Introduce tu texto: ")
-
 # Le decimos al usuario que indique 3 letras de su elección, para poder luego contar cuántas veces aparece en el texto cada una de ellas.
-letra1,letra2,letra3 = (str(input(f"Letra 1: ") + input(f"Letra 2: ") + input(f"Letra 3: ")))
-print(f"Tres letras escogidas, en forma de lista: {letra1.split(),letra2.split(),letra3.split()}")
+lista = []
+lista[0] = input("Escribe la primera letra: ")
+lista[1] = input("Escribe la segunda letra: ")
+lista[2] = input("Escribe la tercera letra: ")
 
 # Contamos cuántas veces aparece cada letra introducida anteriormente para comunicárselo al usuario.
-print(f"La letra '{letra1}' aparece {texto_a_introducir.count(letra1,0)} veces. "
-      f"La letra '{letra2}' aparece {texto_a_introducir.count(letra2,0)} veces. "
-      f"La letra '{letra3}' aparece {texto_a_introducir.count(letra3,0)} veces.\n")
+print(f"La letra '{lista[0]}' aparece {texto_a_introducir.count(lista[0],0)} veces. "
+      f"La letra '{lista[1]}' aparece {texto_a_introducir.count(lista[1],0)} veces. "
+      f"La letra '{lista[2]}' aparece {texto_a_introducir.count(lista[2],0)} veces.\n")
 
 ### Errores cometidos
-print(bool(letra1.count(letra1) == texto_a_introducir.count(letra1)))
+print(bool(lista[0].count(lista[0]) == texto_a_introducir.count(lista[0])))
 
 # Le mostramos al usuario el texto introducido en forma de lista y dividido y la longitud del mismo.
 lista_de_palabras = texto_a_introducir.split()
