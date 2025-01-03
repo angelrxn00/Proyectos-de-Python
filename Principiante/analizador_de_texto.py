2c2ea4154e760749cd89f376d3e43b39e239de7f
Ángel Luis Pajares Carmona <128822525+angelrxn00@users.noreply.github.com>
2025-01-03 22:59:12 +0100

    Update analizador_de_texto.py

--- Principiante/analizador_de_texto.py
+++ Principiante/analizador_de_texto.py
@@ -4,26 +4,27 @@
 Analizador de texto 
 ----------------------------------------------------------------------------------
 El objetivo del proyecto es que, mediante un texto cualquiera que el usuario introduzca y después 3 letras a su elección, analicemos estos 5 casos:
-    - Cuántas veces aparece cada letra (elegida anteriormente), mayúsculas y minúsculas
+    - Cuántas veces aparece cada letra (elegida anteriormente), solo minúsculas
     - Cuántas palabras hay en total
     - Mostrar la primera y la última letra
     - Mostrar las palabras en orden inverso
     - Y, por último, si en el texto aparece la palabra 'python'
 
 """
+
+# Le decimos al usuario que escriba un texto cualquiera para operar sobre él.
+texto_a_introducir = input("Introduce tu texto: ")
+
 # Le decimos al usuario que indique 3 letras de su elección, para poder luego contar cuántas veces aparece en el texto cada una de ellas.
-lista = []
-lista[0] = input("Escribe la primera letra: ")
-lista[1] = input("Escribe la segunda letra: ")
-lista[2] = input("Escribe la tercera letra: ")
+letras = []
+letras.append(input("Escribe la primera letra: ").lower())
+letras.append(input("Escribe la segunda letra: ").lower())
+letras.append(input("Escribe la tercera letra: ").lower())
 
 # Contamos cuántas veces aparece cada letra introducida anteriormente para comunicárselo al usuario.
-print(f"La letra '{lista[0]}' aparece {texto_a_introducir.count(lista[0],0)} veces. "
-      f"La letra '{lista[1]}' aparece {texto_a_introducir.count(lista[1],0)} veces. "
-      f"La letra '{lista[2]}' aparece {texto_a_introducir.count(lista[2],0)} veces.\n")
-
-### Errores cometidos
-print(bool(lista[0].count(lista[0]) == texto_a_introducir.count(lista[0])))
+print(f"La letra '{letras[0]}' aparece {texto_a_introducir.count(letras[0],0)} veces. "
+      f"La letra '{letras[1]}' aparece {texto_a_introducir.count(letras[1],0)} veces. "
+      f"La letra '{letras[2]}' aparece {texto_a_introducir.count(letras[2],0)} veces.\n")
 
 # Le mostramos al usuario el texto introducido en forma de lista y dividido y la longitud del mismo.
 lista_de_palabras = texto_a_introducir.split()
